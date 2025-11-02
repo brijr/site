41952172ea6f1409915065d7d0f8c5f297c27972
Bridger Tower <bridgertower@gmail.com>
2025-11-01 20:24:14 -0600

    add shadcn and craft

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <main>
       <h1>Bridger Tower</h1>
-      <h2>Designer, Software Engineer, and Entrepreneur</h2>
+      <h2 className="text-muted-foreground">
+        Designer, Software Engineer, and Entrepreneur
+      </h2>
     </main>
   );
 }
