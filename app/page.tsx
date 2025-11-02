29edec34a03a944498842e6f000d4a9571c101a4
Bridger Tower <bridgertower@gmail.com>
2025-11-01 20:26:33 -0600

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import { Section, Container } from "@/components/ds";
+
 export default function Home() {
   return (
     <main>
-      <h1>Bridger Tower</h1>
-      <h2 className="text-muted-foreground">
-        Designer, Software Engineer, and Entrepreneur
-      </h2>
+      <Section>
+        <Container>
+          <h1>Bridger Tower</h1>
+          <h2 className="text-muted-foreground">
+            Designer, Software Engineer, and Entrepreneur
+          </h2>
+        </Container>
+      </Section>
     </main>
   );
 }
